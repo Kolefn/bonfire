1e612ad2d11d61659dac73bf8ab5d155f9b919cd
Kole Nunley <Kfnunley@gmail.com>
2020-09-26 21:06:24 -0400

    wrap base game widget in listener

--- lib/base/custom_widget_builder.dart
+++ lib/base/custom_widget_builder.dart
@@ -5,29 +5,21 @@ import 'package:flutter/widgets.dart';
 
 class CustomWidgetBuilder {
   Widget build(Game game) {
-    return ClipRRect(
-      child: Listener(
-        onPointerDown: game is PointerDetector
-            ? (PointerDownEvent d) => (game as PointerDetector).onPointerDown(d)
-            : null,
-        onPointerMove: game is PointerDetector
-            ? (PointerMoveEvent d) => (game as PointerDetector).onPointerMove(d)
-            : null,
-        onPointerUp: game is PointerDetector
-            ? (PointerUpEvent d) => (game as PointerDetector).onPointerUp(d)
-            : null,
-        onPointerCancel: game is PointerDetector
-            ? (PointerCancelEvent d) =>
-                (game as PointerDetector).onPointerCancel(d)
-            : null,
-        child: Container(
-          color: game.backgroundColor(),
-          child: Directionality(
-            textDirection: TextDirection.ltr,
-            child: EmbeddedGameWidget(game),
-          ),
-        ),
-      ),
+    return Listener(
+      onPointerDown: game is PointerDetector
+          ? (PointerDownEvent d) => (game as PointerDetector).onPointerDown(d)
+          : null,
+      onPointerMove: game is PointerDetector
+          ? (PointerMoveEvent d) => (game as PointerDetector).onPointerMove(d)
+          : null,
+      onPointerUp: game is PointerDetector
+          ? (PointerUpEvent d) => (game as PointerDetector).onPointerUp(d)
+          : null,
+      onPointerCancel: game is PointerDetector
+          ? (PointerCancelEvent d) =>
+              (game as PointerDetector).onPointerCancel(d)
+          : null,
+      child: game.widget,
     );
   }
 }
